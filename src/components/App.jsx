5b1e2e29896d461ae1c0e84d1926e8fb17117c1a
kristi <krsy3ii@yahoo.com>
2018-06-29 14:25:04 -0700

    add error 404, edit routes in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
-import Error404 from './Error404';
+import Error404 from './Error404'
+import BrewList from './BrewList'
+import About from './About'
 
 function App(){
   return (
@@ -9,12 +11,9 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={App} />
-        <Route exact path='/list' component={List} />
+        <Route exact path='/about' component={About} />
         <Route component={Error404} />
       </Switch>
-      <h1>React -1st project</h1>
-      <h5>Friday</h5>
-      <p><em>list</em></p>
     </div>
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 function Header(){
   return (
     <div>
-      <h1>React Wk1</h1>
-      <Link to="/">Home</Link> | <Link to="/list">View firstlist </Link> | <Link to="/otherlist">View other list </Link>
+      <h1>Kris' Taps</h1>
+      <Link to="/">Home</Link> | <Link to="/brewlist">View firstlist </Link>
     </div>
   );
 }
