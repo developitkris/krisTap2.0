8d52c4aa528e6bd1cdf3a713e8006bf583f69afb
kristi <krsy3ii@yahoo.com>
2018-06-29 14:58:33 -0700

    add BrewForm component for new beer suggestions, add BrewForm route to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,25 @@ import Header from './Header'
 import { Switch, Route } from 'react-router-dom'
 import Error404 from './Error404'
 import BrewList from './BrewList'
+import BrewForm from './BrewForm'
 import About from './About'
 
 function App(){
   return (
     <div>
+      <style global jsx>{`
+        body {
+          background-size: 85%;
+          background-color: navy;
+          background-position: center;
+          background-repeat: no-repeat;
+        }
+        `}
+      </style>
       <Header/>
       <Switch>
         <Route exact path='/' component={App} />
+        <Route exact path='/brewform' component={BrewForm} />
         <Route exact path='/about' component={About} />
         <Route component={Error404} />
       </Switch>
