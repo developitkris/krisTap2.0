61fa12d04feaef372d539d9f58e2725be03f0f5c
Kristi <krsy3ii@yahoo.com>
2018-07-05 12:13:03 -0700

    create a constructor for NewBrewControl, create click event for BrewSuggestion, add bind method for event

--- src/components/NewBrewControl.jsx
+++ src/components/NewBrewControl.jsx
@@ -7,13 +7,21 @@ class NewBrewControl extends React.Component {
     super(props);
     this.state = {
       formVisibleOnPage: false
+      this.handleClick = this.handleClick.bind(this);
     };
+  }
+
+  handleClick(){
+    this.setState({formVisibleOnPage: true});
+    console.log('You are about to make an awesome brew suggestion. Good thinking.');
+    console.log('formVisibleOnPage is currently set to:' + this.state.formVisibleOnPage);
   }
 
   render(){
     return(
       <div>
         <p>This is the NewBrewControl component</p>
+        <strong onClick={this.handleClick}>Click here to make a suggestion for a new brew!</strong>
       </div>
     )
   }
