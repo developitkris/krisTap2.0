e3fac19a91355b380083a6f7921be509a718918a
Kristi <krsy3ii@yahoo.com>
2018-07-06 11:57:53 -0700

    create SuggestedBrew component, import Moment.js and move over timeOpen function in from Brew

--- src/components/SuggestedBrew.jsx
+++ src/components/SuggestedBrew.jsx
@@ -1,28 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Moment from 'moment';
 
-function Brew(props){
+function SuggestedBrew(props){
   return (
     <div>
       <h3>{props.beer}</h3>
       <ul>
         <li>{props.brewer}</li>
         <li>{props.description}</li>
-        <li>{props.abv}</li>
-        <li>{props.price}</li>
-        <li>{props.keg}</li>
       </ul>
+      <h5>Suggested{displayTimeOpen(props.timeOpen)} ago</h5>
       <hr/>
     </div>
   )}
+
+function displayTimeOpen(timeOpen) {
+  return timeOpen.from(new Moment(), true);
+}
 
 Brew.proptypes = {
   beer: PropTypes.string,
   brewer: PropTypes.string,
   description: PropTypes.arrayOf(PropTypes.string),
-  abv: PropTypes.number,
-  price: PropTypes.number,
-  keg: PropTypes.number,
+  timeOpen: PropTypes.instanceOf(Moment).isRequired
 }
 
-export default Brew;
+export default SuggestedBrew;
